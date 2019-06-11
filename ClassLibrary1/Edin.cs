16724aad7b6794648feed76905b5d1a08e80fee3
Gennadii Shestakov <shestakov.g@gmail.com>
2019-06-11 22:15:29 +0300

    init

---
+++ ClassLibrary1/Edin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EdinLib.EdinServiceReference;
+
+
+namespace EdinLib
+{
+    public class Edin
+    {
+        public Edin()
+        {
+            ServiceWsClient srvClient = new ServiceWsClient();
+            srvClient.Open();
+            getListRequest request = new getListRequest();
+
+            request.user = new ediLogin() {login = "uaunicom", pass = "e50326d7738278dffc3e8d82e73be8f1" };//pass = "e50326d7738278dffc3e8d82e73be8f10"};
+            //request.user.login = "uaunicom";
+            //request.user.pass = "e50326d7738278dffc3e8d82e73be8f1";//uaunicom2
+            var responce = srvClient.getList(request);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.edinsoap;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Edin edin = new Edin();
+            edinsoap.ServiceWsService serv = new ServiceWsService();
+            getListRequest request = new getListRequest();
+
+            request.user = new ediLogin() { login = "uaunicom", pass = "80d10ffd63edf7fd340f120e0fa480a0" };//pass = "e50326d7738278dffc3e8d82e73be8f10"};
+            var resp = serv.getList(request);
+        }
+    }
+}
